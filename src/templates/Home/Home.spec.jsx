7b2882401041a329b6817c802ecfacd7b7ca1f2c
Kevin Costa <kevinsatiro123@gmail.com>
2023-05-16 16:10:02 -0300

    Adicionando testes para a Home

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,5 +1,57 @@
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+const { render, screen, waitForElementToBeRemoved } = require("@testing-library/react");
+import { Home } from '.';
+
+const handlers = [
+  rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
+    return res(ctx.json([
+      {
+        "userId": 1,
+        "id": 1,
+        "title": "Title 1",
+        "body": "Body 1",
+        url: 'img1.jpg',
+      },
+      {
+        "userId": 2,
+        "id": 2,
+        "title": "Title 2",
+        "body": "Body 2",
+        url: 'img2.jpg',
+      },
+      {
+        "userId": 3,
+        "id": 3,
+        "title": "Title 3",
+        "body": "Body 3",
+        url: 'img3.jpg',
+      }
+    ])
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
 describe('<Home />', () => {
-  it('is a dummy test', () => {
-    expect(1).toBe(1);
+  beforeAll(() => {
+    server.listen();
+  })
+
+  afterEach(() => {
+    server.resetHandlers();
+  })
+
+  afterAll(() => {
+    server.close();
+  })
+
+  it('Should render search, posts and load more', async () => {
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts');
+
+    await waitForElementToBeRemoved(noMorePosts);
+    screen.debug();
   });
 });
